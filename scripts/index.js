a37f0ef184b5aea308319df4f3fd27c523b664d1
Maxim Alexeev <C:\Users\\AppData\Roaming\The Bat!>
2023-01-24 03:43:18 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,70 @@
+let main = document.querySelector("main");
+cats.forEach(function (cat) {
+  let card = `<div class="${
+    cat.favourite ? "card like" : "card"
+  }" style="background-image: url(${cat.img_link})">
+     <span>${cat.name}</span>
+     </div>`; 
+  main.innerHTML += card;
+});
+
+let cards = document.querySelectorAll(".card");
+
+for (let i = 0, cnt = cards.length; i < cnt; i++) {
+  const width = cards[i].clientWidth;
+  cards[i].style.height = width * 0.6 + "px";
+}
+
+let addBtn = document.querySelector("#add");
+let popupForm = document.querySelector("#popup-form");
+let closePopupForm = popupForm.querySelector(".popup-close");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!popupForm.classList.contains("active")) {
+    popupForm.classList.add("active");
+    popupForm.parentElement.classList.add("active");
+  }
+});
+
+closePopupForm.addEventListener("click", () => {
+  popupForm.classList.remove("active");
+  popupForm.parentElement.classList.remove("active");
+});
+
+// const api = new Api("biryukov");
+// let form = document.forms[0];
+// form.img_link.addEventListener("change", (e) => {
+//   form.firstElementChild.style.backgroundImage = `url(${e.target.value})`;
+// });
+// form.img_link.addEventListener("input", (e) => {
+//   form.firstElementChild.style.backgroundImage = `url(${e.target.value})`;
+// });
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   let body = {};
+//   for (let i = 0; i < form.elements.length; i++) {
+//     let inp = form.elements[i];
+//     if (inp.type === "checkbox") {
+//       body[inp.name] = inp.checked;
+//     } else if (inp.name && inp.value) {
+//       if (inp.type === "number") {
+//         body[inp.name] = +inp.value;
+//       } else {
+//         body[inp.name] = inp.value;
+//       }
+//     }
+//   }
+//   console.log(body);
+//   api
+//     .addCat(body)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.message === "ok") {
+//         form.reset();
+//         closePopupForm.click();
+//       } else {
+//         console.log(data);
+//       }
+//     });
+// });
